8e3101e391fb6e19c6a6fbdbea62a2381b954a1c
Pavel Zharov <pavel@prna.ru>
2021-10-26 17:19:18 +0700

    fix: :bug: fix header width

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header className='header'>
-      <Link to='/' className='header__logo'></Link>
-      <Link to='/signup' className='header__link header__link_type_signup'>Регистрация</Link>
-      <Link to='/signin' className='header__link header__button button'>Вход</Link>
+      <div className="header__container">
+        <Link to='/' className='header__logo'></Link>
+        <Link to='/signup' className='header__link header__link_type_signup'>Регистрация</Link>
+        <Link to='/signin' className='header__link header__button button'>Вход</Link>
+      </div>
     </header>
   )
 }
